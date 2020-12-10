2919e9a29fb6ddccc3145323c425e22558a4ae15
ian rust <ibrust777@gmail.com>
2020-12-10 01:07:07 -0500

    Initial Commit

---
+++ drawing_app_UIdocument/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  drawing_app_UIdocument
+//
+//  Created by Field Employee on 12/10/20.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
